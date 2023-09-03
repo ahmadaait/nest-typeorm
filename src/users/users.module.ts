e02c6e01c0a0e2a4bd0a3d720d4b0a43b89a0076
ahmadaait <jamezmada@gmail.com>
2023-09-03 23:40:27 +0700

    feat (user): add user service for handling auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Services } from 'src/utils/constants';
+import { IsExist } from 'src/utils/validators/is-exists.validator';
+import { IsNotExist } from 'src/utils/validators/is-not-exists.validator';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
+  providers: [
+    IsExist,
+    IsNotExist,
+    {
+      provide: Services.USERS,
+      useClass: UsersService,
+    },
+  ],
+  exports: [
+    {
+      provide: Services.USERS,
+      useClass: UsersService,
+    },
+  ],
 })
 export class UsersModule {}
