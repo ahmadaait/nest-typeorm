3b3309ad6fc7df665a3e7e53d3d2f71b211dd8c3
ahmadaait <jamezmada@gmail.com>
2023-09-03 23:21:19 +0700

    fix (dependency): add bcryptjs and remove ormconfig

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { Service } from './.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [Service, UsersService],
+  providers: [UsersService],
   controllers: [UsersController],
 })
 export class UsersModule {}
