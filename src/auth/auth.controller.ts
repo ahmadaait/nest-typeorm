f8511a66120023a4df1c782b04f437342b4261a7
ahmadaait <jamezmada@gmail.com>
2023-09-06 23:12:56 +0700

    feat (confirm email): add confirm email for update status to active

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,21 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Inject,
   Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/entities/user.entity';
 import { IUsersService } from 'src/users/users';
 import { Routes, Services } from 'src/utils/constants';
+import { NullableType } from 'src/utils/types/nullable.type';
 import { IAuthService } from './auth';
+import { AuthConfirmEmailDto } from './dtos/auth-confirm-email.dto';
 import { AuthEmailLoginDto } from './dtos/auth-email-login.dto';
 import { AuthRegisterDto } from './dtos/auth-register.dto';
 import { LoginResponseType } from './types/login-response.type';
@@ -30,5 +37,20 @@ export class AuthController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async register(@Body() createUserDto: AuthRegisterDto): Promise<void> {
     return await this.authService.registerUser(createUserDto);
+  }
+
+  @Post('confirm-email')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async confirmEmail(
+    @Body() confirmEmailDto: AuthConfirmEmailDto
+  ): Promise<void> {
+    return this.authService.confirmEmail(confirmEmailDto.hash);
+  }
+
+  @Get('status')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public status(@Request() request): Promise<NullableType<User>> {
+    return this.authService.status(request.user);
   }
 }
